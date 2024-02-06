055c0edc8c5438539855feb5a3cca6f6dde5284f
Evorp <3vorpgaming@gmail.com>
2024-02-05 18:11:11 -0800

    move to discord field in settings.json

--- helpers/getImages.js
+++ helpers/getImages.js
@@ -4,14 +4,14 @@ const settings = require("@resources/settings.json");
  * Convert AttachmentBuilder objects into sendable URLs
  * @author RobertR11, Evorp
  * @param {import("discord.js").Client} client
- * @param {import("discord.js").AttachmentBuilder[]} fileArray files to commit
- * @returns {Promise<string[]>} array of image urls from #submission-spam
+ * @param {import("discord.js").AttachmentBuilder[]} files files to get urls from
+ * @returns {Promise<string[]>} array of image urls
  */
-module.exports = async function getImages(client, ...fileArray) {
+module.exports = async function getImages(client, ...files) {
 	/** @type {import("discord.js").Message} */
 	const imgMessage = await client.channels.cache
-		.get(settings.channels.images)
-		.send({ files: fileArray });
+		.get(settings.discord.channels.images)
+		.send({ files });
 
 	return imgMessage.attachments.map((attachment) => attachment.url);
 };
