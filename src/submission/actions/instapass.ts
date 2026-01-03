b0966f571f148d519239a129246301e2c58e4057
Evorp <75297863+3vorp@users.noreply.github.com>
2026-01-02 18:03:46 -0800

    feat(instapass): multi-embed instapass support

--- src/submission/actions/instapass.ts
+++ src/submission/actions/instapass.ts
@@ -2,7 +2,7 @@ import settings from "@resources/settings.json";
 
 import { randomBytes } from "crypto";
 
-import type { PackFile } from "@interfaces/database";
+import type { Pack, PackFile } from "@interfaces/database";
 
 import getPackByChannel from "@submission/discord/getPackByChannel";
 import { mapSendableMessage } from "@submission/discord/retrieveSubmission";
@@ -19,6 +19,7 @@ import pushTextures from "@submission/results/pushTextures";
 
 import { submissionButtons } from "@helpers/interactions";
 import formattedDate from "@helpers/formattedDate";
+import listify from "@helpers/listify";
 
 import { Message, User, GuildMember, TextChannel } from "discord.js";
 
@@ -30,7 +31,7 @@ const DEBUG = process.env.DEBUG.toLowerCase() === "true";
  * @param message embed to instapass
  * @param member who instapassed it
  */
-export default async function instapass(message: Message, member: User | GuildMember) {
+export default async function instapass(message: Message<true>, member: User | GuildMember) {
 	const packs: PackFile = require("@resources/packs.json");
 	const pack = packs[getPackByChannel(message.channel.id)];
 
@@ -47,19 +48,49 @@ export default async function instapass(message: Message, member: User | GuildMe
 		resultStatus: status,
 	});
 
-	const texture = mapDownloadableMessage(resultMessage);
+	return instapassEmbeds([resultMessage], pack);
+}
+
+export const MAX_SHOWN_TEXTURES = 3;
+
+/**
+ * Instantly pass a series of result messages
+ * @author Evorp
+ * @param messages Messages to instapass
+ * @param pack Pack to instapass to
+ */
+export async function instapassEmbeds(messages: Message<true>[], pack: Pack) {
+	if (!messages.length) return;
+	const textures = messages.map(mapDownloadableMessage);
 
 	// random folder name so multiple textures can be instapassed at once
 	const basePath = `./instapassedTextures/${randomBytes(6).toString("hex")}`;
 
-	const textureInfo = await downloadTexture(texture, pack, basePath);
+	const textureInformation = await Promise.all(
+		textures.map((texture) => downloadTexture(texture, pack, basePath)),
+	);
+
+	const names = textureInformation.map((t) => `[#${t.id}] ${t.name}`);
+
+	// slice if too many
+	const commitNames = listify(
+		names.length > MAX_SHOWN_TEXTURES
+			? [...names.slice(0, MAX_SHOWN_TEXTURES), `${names.length - MAX_SHOWN_TEXTURES} more`]
+			: names,
+	);
 
 	// use allSettled so if one throws the others don't abort
 	await Promise.allSettled([
-		pushTextures(basePath, pack.id, `Instapass ${textureInfo.name} from ${formattedDate()}`),
-		postContributions(generateContributionData(texture, pack)),
-		addContributorRole(message.client, pack, channelOut.guildId, texture.authors),
+		pushTextures(basePath, pack.id, `Instapass ${commitNames} from ${formattedDate()}`),
+		textures.flatMap((texture) => postContributions(generateContributionData(texture, pack))),
+		// guaranteed at least one message from start check
+		addContributorRole(
+			messages[0].client,
+			pack,
+			messages[0].channel.guildId,
+			textures.flatMap((tex) => tex.authors),
+		),
 	]);
 
-	if (DEBUG) console.log(`Texture instapassed: ${message.embeds[0].title}`);
+	if (DEBUG) console.log(`Textures instapassed:\n- ${names.join("\n- ")}`);
 }
